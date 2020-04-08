ef3dbcc09f5a3219e6b9fe83cdcccab20353c3fc
ariel colque <ariel.colque.herrera@gmail.com>
2020-04-08 12:31:18 -0400

    liquidations dialog added

--- app/src/main/java/com/example/home/ui/liquidaciones/LiquidacionFragment.java
+++ app/src/main/java/com/example/home/ui/liquidaciones/LiquidacionFragment.java
@@ -2,14 +2,20 @@ package com.example.home.ui.liquidaciones;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.widget.AppCompatEditText;
+import androidx.appcompat.widget.AppCompatRadioButton;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -17,29 +23,39 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.home.R;
 import com.example.home.ui.AdminDataBase;
+import com.example.home.ui.boleto.ModeloBoleto;
+import com.example.home.ui.home.ModeloSalida;
 import com.example.home.ui.socios.ModeloSocio;
 import com.example.home.ui.socios.SocioAdapter;
 import com.example.home.ui.socios.SociosViewModel;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class LiquidacionFragment extends Fragment {
 
-    private SociosViewModel toolsViewModel;
+    private LiquidacionViewModel liquidacionViewModel;
+    List<ModeloSalida> listaBase;
+    List <String> listaSpinner= new ArrayList<>();
     AdminDataBase adb;
+    ModeloSalida aux;
     private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
-    List <ModeloSocio> listafdb = new ArrayList<ModeloSocio>();
+    List <ModeloBoleto> listafdb = new ArrayList<ModeloBoleto>();
+    HashMap<Integer,Integer> spinnerMap = new HashMap<Integer, Integer>();
+
+    private AppCompatRadioButton actbMale;
+    private AppCompatRadioButton actbFemale;
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        toolsViewModel =
-                ViewModelProviders.of(this).get(SociosViewModel.class);
+        liquidacionViewModel =
+                ViewModelProviders.of(this).get(LiquidacionViewModel.class);
         View root = inflater.inflate(R.layout.fragment_liquidaciones, container, false);
-//        adb = new AdminDataBase(getActivity(),"empresaDeTransporte.db",null, 1);
+        adb = new AdminDataBase(getActivity(),"empresaDeTransporte.db",null, 1);
 //        recyclerView = (RecyclerView)  root.findViewById(R.id.my_recycler_socios);
 //        mAdapter = new SocioAdapter(getActivity(),(ArrayList<ModeloSocio>)adb.listaSocios());
 //        recyclerView.setAdapter(mAdapter);
@@ -48,13 +64,13 @@ public class LiquidacionFragment extends Fragment {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                try{
-                    agregarSocio();
-                }
-                catch (Exception e){
-                    Toast.makeText(getActivity(),
-                "Error", Toast.LENGTH_SHORT).show();
-                };
+   //             try{
+                    realizarLiquidacion();
+//                }
+//                catch (Exception e){
+//                    Toast.makeText(getActivity(),
+//                "Error", Toast.LENGTH_SHORT).show();
+//                };
             }
         });
 
@@ -62,29 +78,78 @@ public class LiquidacionFragment extends Fragment {
         return root;
     }
 
-    void agregarSocio(){
-        Toast.makeText(getActivity(),
-                "Toast por defecto", Toast.LENGTH_SHORT).show();
-//        LayoutInflater inflador = LayoutInflater.from(getActivity());
-//        View view = inflador.inflate(R.layout.dialog_socios ,null, false);
+    void realizarLiquidacion(){
+
+        LayoutInflater inflador = LayoutInflater.from(getActivity());
+        View view = inflador.inflate(R.layout.dialog_liquidaciones ,null, false);
+        final Spinner idSalidaLiquidaciones;
+        idSalidaLiquidaciones = (Spinner) view.findViewById(R.id.spinnerSalidasLiquidacionesD);
 //
-//        final EditText nomb, apel;
-//        nomb = (EditText)view.findViewById(R.id.socioName);
-//        apel = (EditText)view.findViewById(R.id.socioApellido);
-//
-//        AlertDialog.Builder alertAlta = new AlertDialog.Builder(getActivity());
-//        alertAlta.setTitle("Agregar Socio");
-//        alertAlta.setCancelable(false);
-//        alertAlta.setView(view);
-//
-//        alertAlta.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+        final AppCompatEditText acetStatus;
+        //radiobuttons
+//          final AppCompatRadioButton acrbMale;
+//          final AppCompatRadioButton acrbFemale;
+//          acrbMale=(AppCompatRadioButton)view.findViewById(R.id.acrb_male);
+//          acrbFemale=(AppCompatRadioButton)view.findViewById(R.id.acrb_female);
+//        acrbFemale.setOnClickListener(new View.OnClickListener() {
 //            @Override
-//            public void onClick(DialogInterface dialogInterface, int i) {
+//            public void onClick(View v) {
+//                Toast.makeText(getActivity(),
+//                        "female", Toast.LENGTH_SHORT).show();
 //            }
 //        });
-//        alertAlta.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+//        acrbMale.setOnClickListener(new View.OnClickListener() {
 //            @Override
-//            public void onClick(DialogInterface dialogInterface, int i) {
+//            public void onClick(View v) {
+//                Toast.makeText(getActivity(),
+//                        "male", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+        //------radiobuttons
+        AlertDialog.Builder alertAlta = new AlertDialog.Builder(getActivity());
+        alertAlta.setTitle("Realizar liquidacion");
+        alertAlta.setCancelable(false);
+        alertAlta.setView(view);
+
+        listaBase = adb.getListaSalidas();
+        for(int i=0;i<listaBase.size();i++){
+            aux=listaBase.get(i);
+            Log.d("idsalida",":"+aux.getIdSalida());
+            spinnerMap.put(i+1,aux.getIdSalida());
+            String spnDestino = aux.getDestino();
+            String spnFecha = aux.getFechaSalida();
+            String spnHora = aux.getHoraSalida();
+            String nombreCompleto = spnDestino+"   |   "+spnFecha+"   |   "+spnHora;
+            //Log.d("spinner","y :"+nombreCompleto );
+            listaSpinner.add(nombreCompleto);
+        }
+        listaSpinner.add(0,"Salidas Programadas");
+        final Spinner spinnerDestinoOpciones = view.findViewById(R.id.spinnerSalidasLiquidacionesD);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1, listaSpinner);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinnerDestinoOpciones.setAdapter(adapter2);
+        spinnerDestinoOpciones.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+//                String text = adapterView.getItemAtPosition(position).toString();
+//                Toast.makeText(adapterView.getContext(),text,Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+        alertAlta.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+            }
+        });
+
+        alertAlta.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
 //                String id,nom, ape;
 //                int estado=0;
 ////                id = etid.getText().toString().trim();
@@ -100,11 +165,11 @@ public class LiquidacionFragment extends Fragment {
 //                }else{
 //                    Toast.makeText(getActivity(), "Error, campos vacios", Toast.LENGTH_SHORT).show();
 //                }
-//
-//
-//            }
-//        });
-        //alertAlta.show();
+
+
+            }
+        });
+        alertAlta.show();
 
     }
 
---
+++ app/src/main/java/com/example/home/ui/liquidaciones/myRadioButton.java
@@ -0,0 +1,45 @@
+package com.example.home.ui.liquidaciones;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.CompoundButton;
+
+import androidx.appcompat.widget.AppCompatRadioButton;
+
+
+public class myRadioButton extends AppCompatRadioButton {
+    private OnCheckedChangeListener onCheckedChangeListener;
+    public myRadioButton(Context context) {
+        super(context);
+    }
+
+    public myRadioButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public myRadioButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        //this is called when the view is attached
+        setOwnOnCheckedChangeListener();
+        setButtonDrawable(null);//lets remove default drawable to create our own
+
+    }
+    public void setOwnOnCheckedChangeListener(OnCheckedChangeListener onCheckedChangeListener){
+        this.onCheckedChangeListener=onCheckedChangeListener;
+    }
+    private void setOwnOnCheckedChangeListener(){
+        setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                //this is set when we have set our listener .Let's create a method for it
+                if(onCheckedChangeListener !=null) onCheckedChangeListener.onCheckedChanged(buttonView,isChecked);
+                {}
+            }
+        });
+    }
+}
