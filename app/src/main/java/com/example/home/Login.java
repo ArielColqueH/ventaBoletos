9d8b214bae457de80a1577e3ce498db4de4bfbd9
ariel colque <ariel.colque.herrera@gmail.com>
2020-04-11 16:12:00 -0400

    login finished

--- app/src/main/java/com/example/home/Login.java
+++ app/src/main/java/com/example/home/Login.java
@@ -2,8 +2,10 @@ package com.example.home;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -11,16 +13,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity {
     Button loginb;
+    TextView user,password;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
         loginb=(Button)findViewById(R.id.btnLogin);
+        user = (TextView)findViewById(R.id.inputUserName);
+        password=(TextView)findViewById(R.id.inputPassword);
         loginb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                open();
+                String userS,passS;
+                userS=user.getText().toString();
+                passS = password.getText().toString();
+                Log.d("login",userS+":"+passS);
+                if(userS.equals("admin") && passS.equals("admin")){
+                    Toast.makeText(Login.this, "ingresa", Toast.LENGTH_SHORT).show();
+                    open();
+                }else{
+                    Toast.makeText(Login.this, "No ingresa", Toast.LENGTH_SHORT).show();
+                }
+
+
             }
         });
 
@@ -29,6 +45,5 @@ public class Login extends AppCompatActivity {
     {
         Intent intent = new Intent(Login.this, MainActivity.class);
         startActivity(intent);
-        Toast.makeText(Login.this, "Clicked on Button", Toast.LENGTH_LONG).show();
     }
 }
